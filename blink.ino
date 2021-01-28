db8692d472733e4125d9064449e81d66886e679d
bitencourt42 <78155464+bitencourt42@users.noreply.github.com>
2021-01-28 15:14:46 -0300

    Monitor Serial
    Adicionado um texto utilizando o Monitor Serial

--- blink.ino
+++ blink.ino
@@ -1,10 +1,18 @@
 void setup(){
+
 pinMode(2,OUTPUT);
+
+Serial.begin(9600);
+
 }
 
 void loop(){
+
 digitalWrite(2,HIGH);
 delay(1000);
 digitalWrite(2,LOW);
 delay(1000);
+  
+Serial.println("Exibir na tela");
+
 }
